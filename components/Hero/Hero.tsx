d67b345f90732b2701ea55119da65e16badde171
Amodu Kehinde <amodukehindee@gmail.com>
2022-07-05 20:13:09 +0100

    added image

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,23 +1,35 @@
 import React from 'react'
-import { motion} from 'framer-motion'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
 import styles from '../../styles/hero/hero.module.scss'
 import { statData } from '../../mocks/header';
+import {TiImage} from 'react-icons/ti'
 
 export default function Hero() {
   return (
 		<header className={`container ${styles.hero}`}>
 			<div className='row'>
 				<div className={`col-sm-6 ${styles.heroImg}`}>
-					{/* <img src='/images/heroImg.png' alt='bg' /> */}
+					<Image
+						src='/images/heroImg.png'
+						alt='hero'
+						height={500}
+						width={500}
+						objectFit={'contain'}
+						className={styles.Imgblob}
+					/>
 					<h1>NFT</h1>
 				</div>
 				<div className={`col-sm-6 ${styles.heroText}`}>
-					<h1>Discover, and sell Extraordinary <br/> NFTs</h1>
+					<h1>
+						Discover, and sell Extraordinary <br /> NFTs
+					</h1>
 					<span>
 						<img />
 						<img />
 					</span>
 					<motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+						<TiImage /> {''}
 						Explore Now
 					</motion.button>
 					<hr className={styles.lines} />
