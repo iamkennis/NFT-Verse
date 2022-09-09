8fc134a0959f517e56297bf3c0e3052c3e1eca97
Amodu Kehinde <amodukehindee@gmail.com>
2022-09-09 09:47:08 +0100

    add user and auction page

---
+++ mocks/user.ts
@@ -0,0 +1,44 @@
+export const userReview = [
+	{
+		img: '',
+		title: 'Emilie Bulter',
+		tag: '@Emilie',
+		coin: '10.56ETH',
+		dollar: '$10,535.95',
+	},
+	{
+		img: '',
+		title: 'Emilie Bulter',
+		tag: '@Emilie',
+		coin: '10.56ETH',
+		dollar: '$10,535.95',
+	},
+	{
+		img: '',
+		title: 'Emilie Bulter',
+		tag: '@Emilie',
+		coin: '10.56ETH',
+		dollar: '$10,535.95',
+	},
+	{
+		img: '',
+		title: 'Emilie Bulter',
+		tag: '@Emilie',
+		coin: '10.56ETH',
+		dollar: '$10,535.95',
+	},
+	{
+		img: '',
+		title: 'Emilie Bulter',
+		tag: '@Emilie',
+		coin: '10.56ETH',
+		dollar: '$10,535.95',
+	},
+	{
+		img: '',
+		title: 'Emilie Bulter',
+		tag: '@Emilie',
+		coin: '10.56ETH',
+		dollar: '$10,535.95',
+	},
+];
